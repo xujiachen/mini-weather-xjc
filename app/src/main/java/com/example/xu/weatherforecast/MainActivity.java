2e80d876aadb3e22816b392cfdaadde8789c87b8
xujiachen <772090945@qq.com>
2018-10-06 10:09:54 +0800

    init project

---
+++ app/src/main/java/com/example/xu/weatherforecast/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.xu.weatherforecast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/10/6.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
